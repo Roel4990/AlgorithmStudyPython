2895efc2ff9ead8dbca7c4a6abe4127c3bbb588b
roel <roel@contigo.im>
2024-07-11 20:00:36 +0900

    ADD 1920.py

---
+++ 1920.py
@@ -0,0 +1,27 @@
+import sys
+
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+arr = list(map(int, input().split()))
+A.sort()
+
+for num in arr:
+    lt, rt = 0, N - 1
+    isExist = False
+
+    while lt <= rt:
+        mid = (lt + rt) // 2
+        if num == A[mid]:
+            isExist = True
+            print(1)
+            break
+        elif num > A[mid]:
+            lt = mid + 1
+        else:
+            rt = mid - 1
+
+    if not isExist:
+        print(0)
