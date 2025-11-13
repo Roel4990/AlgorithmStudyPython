4aedb3609ca5d2c73ed5ad830b80a28be5d06d01
roel <ash49901@gmail.com>
2025-11-14 03:31:07 +0900

    [Solve] BOJ 24480번 알고리즘 수업 - 깊이 우선 탐색 2

---
+++ 2025-11-12/24480.py
@@ -0,0 +1,23 @@
+import sys
+sys.setrecursionlimit(150000)
+
+def dfs(t):
+    global cnt
+    visited[t] = cnt
+    line[t].sort(reverse=True)
+    for i in line[t]:
+        if visited[i] == 0:
+            cnt += 1
+            dfs(i)
+
+N, M, R = map(int, sys.stdin.readline().split())
+line = [[] for _ in range(N+1)]
+visited = [0]*(N+1)
+cnt = 1
+for _ in range(M):
+    a, b = map(int, sys.stdin.readline().split())
+    line[a].append(b)
+    line[b].append(a)
+dfs(R)
+for i in range(1, N+1):
+    print(visited[i])
