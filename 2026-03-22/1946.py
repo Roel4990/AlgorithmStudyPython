a40087750d9eeab3d5daf7635cff6301a2c14f4a
roel <ash49901@gmail.com>
2026-03-17 18:18:35 +0900

    [Solve] BOJ 1946번

---
+++ 2026-03-22/1946.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+T = int(input())
+
+for _ in range(T):
+    N = int(input())
+    NList = []
+    for _ in range(N):
+        a, b  = map(int, input().split())
+        if b == 1:
+            aFind = a
+        NList.append((a, b))
+    NList.sort(key=lambda x: (x[0]))
+    cnt = 0
+    value = N+1
+    for i in range(0, aFind):
+        if (NList[i][1] <= NList[0][1]) and (NList[i][1] < value):
+            cnt += 1
+            value = NList[i][1]
+    print(cnt)
+
+
+
