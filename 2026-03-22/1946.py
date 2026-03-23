a4ea924b2ed3ab1d71da2a5dc9df7411ce13b200
roel <ash49901@gmail.com>
2026-03-23 23:03:12 +0900

    [Solve] BOJ 1946번 코드 수정

--- 2026-03-22/1946.py
+++ 2026-03-22/1946.py
@@ -9,14 +9,14 @@ for _ in range(T):
     NList = []
     for _ in range(N):
         a, b  = map(int, input().split())
-        if b == 1:
-            aFind = a
+        # if b == 1:
+        #     aFind = a
         NList.append((a, b))
     NList.sort(key=lambda x: (x[0]))
     cnt = 0
     value = N+1
-    for i in range(0, aFind):
-        if (NList[i][1] <= NList[0][1]) and (NList[i][1] < value):
+    for i in range(N):
+        if NList[i][1] < value:
             cnt += 1
             value = NList[i][1]
     print(cnt)
